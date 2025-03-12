70764ed3385f289751001e0156f68504a1165e97
Harry Jung <harryjung135@gmail.com>
2025-03-12 11:06:13 -0600

    done

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { getUsers } from './lib/supabase_crud';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [users, setUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const usersData = await getUsers();
+        setUsers(usersData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color: '#065143', fontSize: 30}}>Lab 6</Text>
+      <View style={styles.list}>
+        {users.map((user) => (
+          <View key={user.id} style={styles.listItem}>
+            <Text style={{color: '#F9F1F3'}}>{user.name} - {user.age}</Text>
+          </View>
+        ))}
+      </View>
+
+
     </View>
   );
 }
@@ -16,5 +40,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  list: {
+    width: '80%',
+    padding: '5%',
+    backgroundColor: '#129490',
+    borderRadius: 7,
+  },
+  listItem: {
+    padding: '4%',
+    marginVertical: '1%',
+    backgroundColor: '#065143',
+    borderRadius: 7,
+    color: '#F9F1F3', 
   },
 });
